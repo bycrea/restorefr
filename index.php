4cc67269133ccc2ba261ad940265146b3d62fa4a
bycrea <yoan.despert@lapiscine.pro>
2019-04-25 12:02:09 +0200

    comments fix

--- index.php
+++ index.php
@@ -23,20 +23,26 @@
     
     // Si la page correspondante au $_GET['p'] existe on inclu son controler
     if(file_exists('controler/'.$_GET['p'].'_c.php')) {
+
         $GLOBALS['p'] = $_GET['p'];
         include('controler/'.$GLOBALS['p'].'_c.php');
+
     } elseif ($_GET['p'] == 400) { 
+
         // Si $_GET['p'] = 400 (erreur interne au programme)
         echo $twig->render('error_400.html', array(
             'title' => 'Re-STORE - Erreur serveur',
             'admin' => $_SESSION['status']
         ));
-    } else { 
+
+    } else {
+
         // Si $_GET['p'] n'existe pas : erreur 404
         echo $twig->render('error_404.html', array(
             'title' => 'Re-STORE - erreur 404',
             'admin' => $_SESSION['status']
         ));
+        
     }
 
 ?>
